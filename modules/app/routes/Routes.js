3c388d5f0caa3779eb28e4614e923c457e4a396e
Matthew Witalinski <mateusz@applifactory.pl>
2015-12-21 21:13:48 +0100

    Projects listing

--- modules/app/routes/Routes.js
+++ modules/app/routes/Routes.js
@@ -12,29 +12,6 @@ module.exports = function(app) {
   app.use(index.helpers);
   app.use(index.texts);
 
-  var enRouter = express.Router();
-  enRouter.route('/').get(index.index);
-  enRouter.route('/projects').get(index.projects);
-  enRouter.route('/projects/:link/:id').get(index.projectDetails);
-  enRouter.route('/solutions').get(index.solutions);
-  enRouter.route('/about-us').get(index.aboutUs);
-  enRouter.route('/people').get(index.people);
-  enRouter.route('/contact')
-    .get(index.contact)
-    .post(index.contact);
-  app.use('', enRouter);
-
-  var plRouter = express.Router();
-  plRouter.route('/').get(index.index);
-  plRouter.route('/projekty').get(index.projects);
-  plRouter.route('/projekty/:link/:id').get(index.projectDetails);
-  plRouter.route('/rozwiazania').get(index.solutions);
-  plRouter.route('/o-nas').get(index.aboutUs);
-  plRouter.route('/ludzie').get(index.people);
-  plRouter.route('/kontakt')
-    .get(index.contact)
-    .post(index.contact);
-  app.use('/pl', plRouter);
 
   var adminRouter = express.Router();
   adminRouter.route('')
@@ -42,5 +19,31 @@ module.exports = function(app) {
     .post(admin.login);
   adminRouter.route('/logout').get(admin.logout);
   app.use('/admin', adminRouter);
+
+
+  var plRouter = express.Router();
+  plRouter.route('/').get(index.index);
+  // plRouter.route('/projekty/:link/:id').get(index.projectDetails);
+  // plRouter.route('/rozwiazania').get(index.solutions);
+  // plRouter.route('/o-nas').get(index.aboutUs);
+  // plRouter.route('/ludzie').get(index.people);
+  plRouter.route('/kontakt')
+    .get(index.contact)
+    .post(index.contact);
+  plRouter.route('/:category').get(index.projects);
+  app.use('/pl', plRouter);
+
+
+  var enRouter = express.Router();
+  enRouter.route('/').get(index.index);
+  // enRouter.route('/projects/:link/:id').get(index.projectDetails);
+  // enRouter.route('/solutions').get(index.solutions);
+  // enRouter.route('/about-us').get(index.aboutUs);
+  // enRouter.route('/people').get(index.people);
+  enRouter.route('/contact')
+    .get(index.contact)
+    .post(index.contact);
+  enRouter.route('/:category').get(index.projects);
+  app.use('', enRouter);
 
 };
